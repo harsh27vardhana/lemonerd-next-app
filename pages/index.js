a2dd1ccf9a60e7d74acf7d54e008720acbd3865c
sinha-anubhav79 <sinha.anubhav.79@gmail.com>
2021-06-02 11:22:51 +0530

    Object of schema created, date dropdown added and caption input added. Created card for article on home page

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,23 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Button from 'react-bootstrap/Button'
+import Head from "next/head";
+import { CardColumns } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import ArticleCard from "../components/articleCard";
 
 export default function Home() {
   return (
-    <div >
-     <Head>
-            <title>Welcome To Lemonerd</title>
-  
-            
-     </Head>
-     <Button className='btn-danger'>Button</Button>
+    <div>
+      <Head>
+        <title>Welcome To Lemonerd</title>
+      </Head>
+      <Button className="btn-danger">Button</Button>
+      <CardColumns>
+        <ArticleCard />
+        <ArticleCard />
+        <ArticleCard />
+        <ArticleCard />
+        <ArticleCard />
+        <ArticleCard />
+      </CardColumns>
     </div>
-  )
+  );
 }
