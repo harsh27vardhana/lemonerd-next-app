b8663ce13ad7c84f1343efb41b51412ade548acc
harsh27vardhana <63390956+harsh27vardhana@users.noreply.github.com>
2021-06-22 21:09:46 +0530

    author id added

--- pages/api/authors/index.js
+++ pages/api/authors/index.js
@@ -1,12 +1,22 @@
 const file = "./data/authors.json"
 import Data from "../../../data/authors.json";
 const fs=require('fs')
+import { v4 as uuidv4 } from 'uuid';
 export default async (req, res) => {
-    Data.authors.push(req.body);
+   
     try {
-        // console.log(req.body);
+        console.log(req.body);
+        const authorid = Date.now() + uuidv4();
+        const data={
+            id:authorid,
+            description: req.body.description,
+            image:req.body.image,
+            name:req.body.name
+
+        }
+        Data.authors.push(data);
         fs.writeFile(file, JSON.stringify(Data,null,2),(e)=>{
-            console.log(e);
+            // console.log(e);
         });
         res.send({ 'sucess': 'true' });
     }
