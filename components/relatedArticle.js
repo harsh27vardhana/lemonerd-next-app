5a029a5a206ea64c834da615f9dfbefca2dcba5d
sinha-anubhav79 <sinha.anubhav.79@gmail.com>
2021-07-10 14:42:43 +0530

    enhancements

--- components/relatedArticle.js
+++ components/relatedArticle.js
@@ -1,5 +1,5 @@
 import { Card, Button, Col, Row } from "react-bootstrap";
-import style from "../styles/articleCard.module.css";
+import style from "../styles/relatedArticle.module.css";
 import Link from "next/link";
 import Author from "../data/authors.json";
 
@@ -35,35 +35,25 @@ function RelatedArticle(props) {
   };
 
   return (
-    <Card
-      className="m-2"
-      style={{
-        boxShadow:
-          "rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
-      }}
-    >
+    <Card className={style.cardMain}>
       <div
+        className={style.cardImg}
         style={{
-          backgroundSize: "cover",
-          backgroundPosition: "center",
           backgroundImage: `url("${props.thumbnail.replace(/%2F/gi, "/")}")`,
-          height: "25vw",
-          width: "100%",
         }}
       />
       <div className="d-flex align-items-stretch" style={{ height: "100%" }}>
         <Card.Body>
           <Link href="/posts/[post_id]" as={`/posts/${props._id}`}>
-            <Card.Title role="button" style={{ fontSize: "2vw" }}>
-              {props.title.substring(0, 20) +
-                (props.title.length > 19 ? "..." : "")}
+            <Card.Title role="button" className={style.cardTitle}>
+              {props.title.substring(0, 30) +
+                (props.title.length > 29 ? "..." : "")}
             </Card.Title>
           </Link>
-          <Card.Text>
-            <small className="text-muted">Posted {getDate()}</small>
-          </Card.Text>
           <footer className="blockquote-footer my-0">
             <small className="text-muted">
+              Posted {getDate()}
+              <br />
               Written by{" "}
               <cite title="Source Title" className="author-anchor">
                 <Link href="/authors/[author]" as={`/authors/${props.author}`}>
