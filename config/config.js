9df188e61b0142ed4644630abfba5c4888eb10c4
harsh27vardhana <63390956+harsh27vardhana@users.noreply.github.com>
2021-07-11 14:21:39 +0530

    vercel_url2

--- config/config.js
+++ config/config.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = process.env.NODE_ENV === "development" ? process.env.SERVER_URI: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+export const server = process.env.NODE_ENV === "development" ? 'http://localhost:3000': `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
